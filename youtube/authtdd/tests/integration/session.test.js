445f1efc9a149f8e6daf16785a6ed0041421a8ed
Armando Tomazzoni Jr <armando@agenciaade.com.br>
2019-09-17 11:43:35 -0300

    video sobre auth + tdd: https://youtu.be/2G_mWfG0DZE

--- youtube/authtdd/tests/integration/session.test.js
+++ youtube/authtdd/tests/integration/session.test.js
@@ -1,18 +1,16 @@
 const request = require("supertest");
 
 const app = require("../../src/app");
-const { User } = require("../../src/app/models");
 const truncate = require("../../tests/utils/truncate");
+const factory = require("../factories");
 
-describe("Auth", () => {
+describe("Authorization", () => {
   beforeEach(async () => {
     await truncate();
   });
 
   it("deve autenticar com login/senha", async () => {
-    const user = await User.create({
-      name: "Armando",
-      email: "armando@agenciaade.com.br",
+    const user = await factory.create("User", {
       password: "123123"
     });
 
@@ -26,9 +24,21 @@ describe("Auth", () => {
   });
 
   it("nao autenticar com senha errada", async () => {
-    const user = await User.create({
-      name: "Armando",
-      email: "armando@agenciaade.com.br",
+    const user = await factory.create("User", {
+      password: "senhaerrada"
+    });
+
+    const response = await request(app)
+      .post("/sessions")
+      .send({
+        email: user.email,
+        password: "123123"
+      });
+    expect(response.status).toBe(401);
+  });
+
+  it("deve retornar um jwt token", async () => {
+    const user = await factory.create("User", {
       password: "123123"
     });
 
@@ -38,6 +48,38 @@ describe("Auth", () => {
         email: user.email,
         password: "123123"
       });
+    expect(response.body).toHaveProperty("token");
+  });
+
+  it("deve acessar rotas privadas com token", async () => {
+    const user = await factory.create("User", {
+      password: "123123"
+    });
+
+    const response = await request(app)
+      .get("/dashboard")
+      .set("Authorization", `Bearer ${user.generateToken()}`);
+
+    expect(response.status).toBe(200);
+  });
+
+  it("não deve acessar rotas privadas sem token", async () => {
+    const user = await factory.create("User", {
+      password: "123123"
+    });
+
+    const response = await request(app)
+      .get("/dashboard")
+      .set("Authorization", `Bearer ${user.generateToken()}`);
+
+    expect(response.status).toBe(200);
+  });
+
+  it("não deve acessar rotas privadas com token invalido", async () => {
+    const response = await request(app)
+      .get("/dashboard")
+      .set("Authorization", `Bearer 123`);
+
     expect(response.status).toBe(401);
   });
 });
