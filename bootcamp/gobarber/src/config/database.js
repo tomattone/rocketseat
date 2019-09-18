773804af33f4115a07063015bf092b3abbf42d47
Armando Tomazzoni Jr <armando@agenciaade.com.br>
2019-09-18 15:22:40 -0300

    inicio backend bootcamp - gobarber

---
+++ bootcamp/gobarber/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'tuffi.db.elephantsql.com',
+  username: 'cnbsaqpj',
+  password: 'u1dXcJg83d8MSHExWQMlrxuV2o7Wf4ix',
+  database: 'cnbsaqpj',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
