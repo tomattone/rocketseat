589882bb4bc21566efd41b95ccf4caf67d6a4d62
Armando Tomazzoni Jr <armando@agenciaade.com.br>
2019-10-01 12:13:06 -0300

    Omnistack9 dia 2

---
+++ omnistack-09/backend/src/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+const app = express();
+
+mongoose.connect(
+  "mongodb+srv://root:root@cluster0-lzqb7.mongodb.net/omnistack9?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
